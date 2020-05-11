c1acfc0f3b394a9c86522adb2f74a82efafb69b0
Max van de Laar <max.vandelaar@me.com>
2020-05-11 19:21:13 +0200

    Added logo for windows Prevent auto open dev console

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 async function checkForUpdate(){
